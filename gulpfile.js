5a0e7eec4cef4fb50a05075b21fb900f7b737bca
annaindistress <annaindistress@gmail.com>
2022-06-01 15:08:16 +0300

    Adding sprite

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
 const csso = require('gulp-csso');
+const svgSprite = require('gulp-svg-sprite');
 const del = require('del');
 const ghpages = require('gh-pages');
 const sync = require('browser-sync');
@@ -19,11 +20,53 @@ gulp.task('copy', function () {
         .src([
             'src/*.html',
             'src/fonts/**/*',
-            'src/images/**/*'
+            'src/images/*.png',
+            'src/images/*.svg'
         ], {
             base: 'src'
         })
         .pipe(gulp.dest('dist'))
+        .pipe(sync.stream());
+});
+
+// Sprite
+
+gulp.task('sprite', function () {
+    return gulp
+        .src('src/images/sprite/*.svg')
+        .pipe(svgSprite({
+            shape: {
+                dimension: {
+                    maxWidth: 500,
+                    maxHeight: 500
+                },
+                spacing: {
+                    padding: 0
+                },
+                transform: [{
+                    "svgo": {
+                        "plugins": [
+                            { removeViewBox: false },
+                            { removeUnusedNS: false },
+                            { removeUselessStrokeAndFill: true },
+                            { cleanupIDs: false },
+                            { removeComments: true },
+                            { removeEmptyAttrs: true },
+                            { removeEmptyText: true },
+                            { collapseGroups: true },
+                            { removeAttrs: { attrs: '(fill|stroke|style)' } }
+                        ]
+                    }
+                }]
+            },
+            mode: {
+                symbol: {
+                    dest : '.',
+                    sprite: 'sprite.svg'
+                }
+            }
+        }))
+        .pipe(gulp.dest('dist/images/'))
         .pipe(sync.stream());
 });
 
@@ -57,8 +100,9 @@ gulp.task('watch', function() {
     gulp.watch([
         'src/*.html',
         'src/fonts/**/*',
-        'src/images/**/*'
+        'src/images/*'
     ], gulp.series('copy'));
+    gulp.watch('src/images/sprite/*', gulp.series('sprite'));
     gulp.watch('src/styles/**/*.less', gulp.series('styles'));
 });
 
@@ -67,6 +111,7 @@ gulp.task('watch', function() {
 gulp.task('build', gulp.series(
     'clean',
     'copy',
+    'sprite',
     'styles'
 ));
 
